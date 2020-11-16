68ad162f22d8d67571abce83c84bc1a393957965
Robert Horngren <robert@horngren.net>
2020-11-16 11:51:03 +0100

    added fuction to add items

--- app/src/main/java/com/example/roomservicereminder/MainActivity.kt
+++ app/src/main/java/com/example/roomservicereminder/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.roomservicereminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.room.Room
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
@@ -14,6 +18,8 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
     private lateinit var job : Job
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
+
+    lateinit var addTaskEditText : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,27 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "toDo-items")
             .fallbackToDestructiveMigration().build()
 
+        addTaskEditText = findViewById(R.id.addTaskEditText)
 
+        val addButton = findViewById<Button>(R.id.addButton)
+
+
+        addButton.setOnClickListener{
+            addToDoItem(Item(0, "${addTaskEditText.text}", false))
+        }
+
+
+
+    }
+
+    fun addToDoItem(item: Item) {
+        launch(Dispatchers.IO) {
+            db.itemDao().insert(item)
+        }
+    }
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
     }
 }
