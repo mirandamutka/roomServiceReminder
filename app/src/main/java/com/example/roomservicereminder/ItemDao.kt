aa50b6f7ffc2639ff098f069d5c1a388e30167b2
Robert Horngren <robert@horngren.net>
2020-11-16 10:59:20 +0100

    added foundation for Room

---
+++ app/src/main/java/com/example/roomservicereminder/ItemDao.kt
@@ -0,0 +1,20 @@
+package com.example.roomservicereminder
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ItemDao {
+
+    @Insert
+    fun insert(item: Item)
+
+    @Delete
+    fun delete(item: Item)
+
+    @Query("SELECT * FROM item")
+    fun getAll() : List<Item>
+
+}
