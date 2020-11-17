18be46d7bd50d53153cbda5a942a00396bf63ac6
Robert Horngren <robert@horngren.net>
2020-11-17 10:57:01 +0100

    added callback when saving items

--- app/src/main/java/com/example/roomservicereminder/Item.kt
+++ app/src/main/java/com/example/roomservicereminder/Item.kt
@@ -5,8 +5,8 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Item (
-    @PrimaryKey(autoGenerate = true) val id : Int,
-    @ColumnInfo(name = "task") var task : String?,
-    @ColumnInfo(name = "done") var done : Boolean = false
+data class Item(
+        @PrimaryKey(autoGenerate = true) val id: Int,
+        @ColumnInfo(name = "task") var task: String?,
+        @ColumnInfo(name = "done") var done: Boolean = false
 )
